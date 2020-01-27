c0b350c693d5a1d6f09d802faa634b84f755e569
Shivashankar P <shivashankar.ror@gmail.com>
2020-01-27 18:06:28 +0530

    Added REST update method for donationaction

--- lib/actionkit_connector/rest/donation_action.rb
+++ lib/actionkit_connector/rest/donation_action.rb
@@ -6,6 +6,10 @@ module ActionKitConnector
         self.class.get("/donationaction/#{id}", basic_auth)
       end
 
+      def update_donation_action(id, data)
+        self.class.put("/donationaction/#{id}/", prep_options(data))
+      end
+
       def delete_donation_action(id)
         self.class.delete("/donationaction/#{id}/", basic_auth)
       end
