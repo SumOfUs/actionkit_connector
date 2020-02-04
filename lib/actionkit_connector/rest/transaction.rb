dbe8ef631aae41647bae83288efd767a4991c9c5
Shivashankar P <shivashankar.ror@gmail.com>
2020-02-04 12:56:11 +0530

    Added api to fetch order and transaction details

--- lib/actionkit_connector/rest/transaction.rb
+++ lib/actionkit_connector/rest/transaction.rb
@@ -1,6 +1,9 @@
 module ActionKitConnector
   module REST
     module Transaction
+      def get_transaction(id)
+        self.class.get("/transaction/#{id}/", prep_options({}))
+      end
 
       def update_transaction(id, data)
         self.class.put("/transaction/#{id}/", prep_options(data))
