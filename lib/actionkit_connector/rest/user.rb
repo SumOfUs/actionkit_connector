a3cf46f377682902e64820c3adc575f9212474b5
Shivashankar P <shivashankar.ror@gmail.com>
2020-04-20 20:45:14 +0530

    Find user donations by email

--- lib/actionkit_connector/rest/user.rb
+++ lib/actionkit_connector/rest/user.rb
@@ -4,6 +4,11 @@ module ActionKitConnector
       def update_user(id, data)
         self.class.put("/user/#{id}/", prep_options(data))
       end
+
+      def filter_user_orders_by(field: 'user__email', value: '')
+        return [] if field.to_s.empty? || value.to_s.empty?
+        self.class.get("/order/?#{field}=#{value}", prep_options({}))
+      end
     end
   end
 end
